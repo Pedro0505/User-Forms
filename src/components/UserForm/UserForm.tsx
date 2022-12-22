bbe765cb4a58cb7e95b5f98676b9a6ea19368a9f
pedro <pedrohrds05@hotmail.com>
2022-12-22 18:06:26 -0300

    feat: adding header to create user

---
+++ src/components/UserForm/UserForm.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import { RiFileTextFill, RiHome4Fill, RiUser5Fill } from 'react-icons/ri';
+import UserIdentification from '../UserIdentification/UserIdentification';
+import './style.css';
+
+function UserForm() {
+  return (
+    <main className="user-form-main">
+      <h1>Criação de usuário</h1>
+      <div className="icons-group">
+        <span>
+          <div>
+            <RiUser5Fill fontSize="25px" />
+          </div>
+          <p>Identificação do Usuário</p>
+        </span>
+        <span>
+          <div>
+            <RiHome4Fill fontSize="25px" />
+          </div>
+          <p>Endereço do Usuário</p>
+        </span>
+        <span>
+          <div>
+            <RiFileTextFill fontSize="25px" />
+          </div>
+          <p>Sobre Você</p>
+        </span>
+      </div>
+      <UserIdentification />
+    </main>
+  );
+}
+
+export default UserForm;
