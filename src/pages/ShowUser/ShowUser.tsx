47f83777a68f855ff005dcc3d98e1881e083337c
pedro <pedrohrds05@hotmail.com>
2022-12-24 17:49:31 -0300

    feat: make redirect when user info is empty

--- src/pages/ShowUser/ShowUser.tsx
+++ src/pages/ShowUser/ShowUser.tsx
@@ -1,8 +1,19 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 import ShowUserModal from '../../components/ShowUserModal/ShowUserModal';
+import { UserContext } from '../../context/UserContext';
 import './style.css';
 
 function ShowUser() {
+  const { userInfo } = useContext(UserContext);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (userInfo.name === '') {
+      navigate('/');
+    }
+  });
+
   return (
     <main className="show-user-main">
       <ShowUserModal />
