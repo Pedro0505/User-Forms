d1f806006057fe0c8aee3946e27e41902b81fb85
pedro <pedrohrds05@hotmail.com>
2022-12-24 10:26:03 -0300

    feat: adding react router dom in project

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import { UserProvider } from './context/UserContext';
 import CreateUser from './pages/CreateUser/CreateUser';
+import ShowUser from './pages/ShowUser/ShowUser';
 import './style/App.css';
 
 function App() {
   return (
     <div>
-      <CreateUser />
+      <UserProvider>
+        <Routes>
+          <Route path="/" element={ <CreateUser /> } />
+          <Route path="/created" element={ <ShowUser /> } />
+        </Routes>
+      </UserProvider>
     </div>
   );
 }
